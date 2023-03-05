508633b294f9a6cca5d42b863fd5caeb32fa941a
Sergey Sudakov <sergey.sudakov@ontravelsolutions.com>
2023-03-05 23:00:06 +0300

    add catalogy btn

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,9 @@ const contactsBtn = document.querySelector('button.tel');
 contactsBtn.addEventListener('click', (event) => {
     event.currentTarget.classList.toggle('opened')
 });
+
+const catalogyBtn = document.querySelector('button.catalogy');
+
+catalogyBtn.addEventListener('click', (event) => {
+    event.currentTarget.classList.toggle('opened')
+})
