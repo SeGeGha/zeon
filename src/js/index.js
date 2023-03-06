7ca73e310857bffa6ac6f7805c280035abb1ac6f
Sergey Sudakov <sergey.sudakov@ontravelsolutions.com>
2023-03-06 19:57:01 +0300

    refactor header components

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
-const contactsBtn = document.querySelector('button.tel');
+const contactsBtns = document.querySelectorAll('button.tel');
 
-contactsBtn.addEventListener('click', (event) => {
-    event.currentTarget.classList.toggle('opened')
-});
+contactsBtns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        const contactsWrap = event.target.closest('.contacts');
+
+        contactsWrap.classList.toggle('opened')
+    });
+})
 
 const catalogBtn = document.querySelector('.catalog-btn');
 
@@ -10,4 +14,8 @@ catalogBtn.addEventListener('click', (event) => {
     const catalogWrap = event.target.closest('.catalog');
 
     catalogWrap.classList.toggle('opened');
+});
+
+window.addEventListener('scroll', (event) => {
+    document.querySelector('#header').classList.toggle('fixed', !!window.scrollY);
 })
