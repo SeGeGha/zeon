5cfc52c6ea38ffce83f79330b2f481b0b9190b18
Sergey Sudakov <sergey.sudakov@ontravelsolutions.com>
2023-03-07 20:42:08 +0300

    add styles for mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -18,4 +18,24 @@ catalogBtn.addEventListener('click', (event) => {
 
 window.addEventListener('scroll', (event) => {
     document.querySelector('#header').classList.toggle('fixed', !!window.scrollY);
+});
+
+const menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click', (event) => {
+   const menuWrap = event.target.closest('.menu');
+
+   document.body.classList.add('locked');
+   menuWrap.classList.toggle('opened');
+});
+
+const closeBtns = document.querySelectorAll('.close-btn')
+
+Array.from(closeBtns).forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        const wrap = event.target.closest('.opened');
+
+        wrap.classList.toggle('opened');
+        document.body.classList.remove('locked');
+    })
 })
